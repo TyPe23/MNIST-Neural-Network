be0b93a7bdfd6f8d0bccea93c105738aa8814a0d
Ty Pederson <62142901+TyPe23@users.noreply.github.com>
2022-10-12 21:18:46 -0500

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -10,33 +10,54 @@
 
 // import necessary packages
 import java.io.*;
-
 import java.util.Scanner;
+import java.lang.Math;
 
 // main class
 public class Assignment2 {
 
+	public static int numLayers;
+	public static double[][] biases = new double[2][];
+	public static double[][] weights = new double[3][];
+
 	// start here
 	public static void main(String[] args) throws Exception{
-		System.out.println("Hello world!");
 
 		// create a new file from the .csv training file
-		Scanner sc = new Scanner(new File(System.getProperty("user.dir") + "\\mnist_train.csv"));
+		Scanner train = new Scanner(new File(System.getProperty("user.dir") + "\\mnist_train.csv"));
+		Scanner test = new Scanner(new File(System.getProperty("user.dir") + "\\mnist_test.csv"));
 
 		// set the delimiter to a comma
-		sc.useDelimiter(",");
+		train.useDelimiter(",");
+		test.useDelimiter(",");
 
-		// print out all of the values to make sure it works
+		/* // print out all of the values to make sure it works
 		int i = 0;
 
 		while (i < 783 * 3 + 4) {
-			if (sc.hasNext()) {
-				System.out.println(sc.next());
+			if (train.hasNext()) {
+				System.out.print(train.next());
 			}
 			i++;
 		}
 
 		// close the .csv file before exiting
-		sc.close();
+		train.close();
+		test.close(); */
+		int[] nodesPerLayer = {2,3,1};
+		Network(nodesPerLayer);
+	}
+
+	public static void Network(int[] nodes) {
+		numLayers = nodes.length;
+		
+		for(int i = 0; i < numLayers - 1; i++) {
+			biases[i] = new double[nodes[i + 1]];
+			for(int j = 0; j < nodes[i + 1]; j++) {
+				biases[i][j] = Math.random() * 2 - 0.5;
+				System.out.print(biases[i][j] + " ");
+			}
+			System.out.println("");
+		}
 	}
 }
