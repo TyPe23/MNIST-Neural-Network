9721717faae92db37e89096c158a1403881c0933
Ty Pederson <62142901+TyPe23@users.noreply.github.com>
2022-10-12 23:18:10 -0500

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -18,7 +18,7 @@ public class Assignment2 {
 
 	public static int numLayers;
 	public static double[][] biases = new double[2][];
-	public static double[][] weights = new double[3][];
+	public static double[][][] weights = new double[3][][];
 
 	// start here
 	public static void main(String[] args) throws Exception{
@@ -44,7 +44,7 @@ public class Assignment2 {
 		// close the .csv file before exiting
 		train.close();
 		test.close(); */
-		int[] nodesPerLayer = {2,3,1};
+		int[] nodesPerLayer = {784,15,10};
 		Network(nodesPerLayer);
 	}
 
@@ -52,15 +52,23 @@ public class Assignment2 {
 	public static void Network(int[] nodes) {
 		numLayers = nodes.length;
 		
+		// for loop that creates the biases
 		for(int i = 0; i < numLayers - 1; i++) {
+			// allocates the memory for each layers' biases
 			biases[i] = new double[nodes[i + 1]];
 			for(int j = 0; j < nodes[i + 1]; j++) {
 				biases[i][j] = Math.random() * 2 - 1;
-				//System.out.print(biases[i][j] + " ");
+				System.out.print(biases[i][j] + " ");
 			}
-			// System.out.println("");
+			System.out.println("");
 		}
 
-		
+		// for loop that creates the weights
+		for(int i = 0; i < numLayers - 1; i++) {
+			// allocates the memory for each layers' weights
+			weights[i] = new double[nodes[i]][nodes[i + 1]];
+			System.out.println(weights.length);
+			//for(int j = 0; j < )
+		}
 	}
 }
