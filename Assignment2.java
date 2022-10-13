259d1ac4575ee50724e2de1ffc5069122f7405e0
Ty Pederson <ty23117@gmail.com>
2022-10-13 14:59:25 -0500

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -16,11 +16,16 @@ import java.lang.Math;
 // main class
 public class Assignment2 {
 
-	public static int[][] training_data;
-	public static int[][] testing_data;
-	public static int numLayers;
+	// 2D arrays containing training and testing data
+	public static int[][] training_data = new int[60000][785];
+	public static int[][] testing_data = new int[10000][785];
+
+	// indices of training data array
+	public static int[] training_indices = new int[60000];
+
+	// multidementional arrays of weights and biases
 	public static double[][] biases = new double[2][];
-	public static double[][][] weights = new double[3][][];
+	public static double[][][] weights = new double[2][][];
 
 	// start here
 	public static void main(String[] args) throws Exception{
@@ -30,8 +35,8 @@ public class Assignment2 {
 		Scanner test = new Scanner(new File(System.getProperty("user.dir") + "\\mnist_test.csv"));
 
 		// set the delimiter to a comma
-		train.useDelimiter(",");
-		test.useDelimiter(",");
+		train.useDelimiter(",|\\n");
+		test.useDelimiter(",|\\n");
 
 		// put all of the values of the training data into a 2D array
 		int i = 0;
@@ -39,6 +44,7 @@ public class Assignment2 {
 		while (train.hasNext()) {
 			training_data[i][j] = Integer.valueOf(train.next());
 			j++;
+
 			if (j == 785){
 				i++;
 				j = 0;
@@ -51,10 +57,16 @@ public class Assignment2 {
 		while (test.hasNext()) {
 			testing_data[i][j] = Integer.valueOf(test.next());
 			j++;
+
 			if (j == 785){
 				i++;
 				j = 0;
 			}
+		}
+
+		i = 0;
+		while (i < 60000) {
+			training_indices[i] = i;
 		}
 
 		// close the .csv file before exiting
@@ -66,31 +78,39 @@ public class Assignment2 {
 
 	// function that creates the weights and biases for the network
 	public static void Network(int[] nodes) {
-		numLayers = nodes.length;
-		
-		// for loop that creates the biases
-		for(int i = 0; i < numLayers - 1; i++) {
+		// biases
+		// for loop that iterates through each layer
+		for(int i = 0; i < nodes.length - 1; i++) {
 			// allocates the memory for each layers' biases
 			biases[i] = new double[nodes[i + 1]];
 
-			// for loop that assigns values to each of the biases
+			// for loop iterates through each node
 			for(int j = 0; j < nodes[i + 1]; j++) {
-
-				// random values from -1 to 1
+				// random values from -1 to 1 
 				biases[i][j] = Math.random() * 2 - 1;
-				System.out.print(biases[i][j] + " ");
 			}
-			System.out.println("");
 		}
 
-		// for loop that creates the weights
-		for(int i = 0; i < numLayers - 1; i++) {
+		// weights
+		// for loop that iterates through each layer
+		for(int i = 0; i < nodes.length - 1; i++) {
 			// allocates the memory for each layers' weights
 			weights[i] = new double[nodes[i]][nodes[i + 1]];
-			
-			for(int j = 0; j < nodes[i]; i++) {
 
+			System.out.print("Layer " + i);
+
+			// for loop that iterates through each node
+			for(int j = 0; j < weights[i].length; j++) {
+				
+				System.out.print(" Node: " + j);
+				// for loop that iterates through each weight
+				for(int k = 0; k < weights[i][j].length; k++) {
+					// random value from -1 to 1
+					weights[i][j][k] = Math.random() * 2 - 1;
+				}
+				System.out.print(" Weights: " + weights[i][j].length);
 			}
+			System.out.println("");
 		}
 	}
 }
